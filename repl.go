bd4e1e2184ef1f9e57a6269ec1c31c8934314c52
BTVance <blaketannehill@gmail.com>
2026-05-20 14:04:30 -0700

    repl prints first command back to the user

--- repl.go
+++ repl.go
@@ -1,10 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	text = strings.TrimSpace(text)
 	format_text := strings.Fields(text)
 	return format_text
+}
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		text := scanner.Text()
+		cleanText := cleanInput(text)
+		if len(cleanText) > 0 {
+			fmt.Printf("Your command was: %s\n", cleanText[0])
+		}
+	}
 }
